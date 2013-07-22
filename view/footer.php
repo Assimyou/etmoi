05b3e2cae0eb2a877c4079edf9eeb4610b877bc7
Elwann <lojic@hotmail.fr>
2013-07-22 22:23:39 +0200

    fix vignettes

---
+++ view/footer.php
@@ -0,0 +1,41 @@
+		<!-- Debut du footer -->
+		<footer class="group">
+			<div class="center">
+				<div class="sitemap">
+					<h3>Montrouge et moi</h3>
+					<nav class="footer-menu">
+						<ul>
+							<li></li>
+						</ul>
+					</nav>
+				</div>
+
+				<div class="top-associations">
+					<h3>Top associations</h3>
+					<ul>
+						<li></li>
+					</ul>
+				</div>
+
+				<div class="login">
+					<h3>Login</h3>
+					<input type="text" />
+					<input type="password" />
+				</div>
+			</div>
+		</footer>
+		<!-- Fin du footer -->
+
+
+		<!-- Debut des scripts. 
+		<script src="js/main.js"></script>
+
+		Google Analytics: ne pas oublier de mettre l'ID Correcte. 
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src='//www.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script> -->
+	</body>
+</html>
--- view/home.php
+++ view/home.php
@@ -1,50 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"><!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>Montrouge et Moi</title>
-		<meta name="description" content="Mon site des assoctiations et évenements de Montrouge">
-		<meta name="viewport" content="width=device-width">
-
-		<link rel="stylesheet" href="style/normalize.css">
-		<link rel="stylesheet" href="style/biere.css">
-		<!--[if lt IE 9]>
-			<script src="js/lib/html5shiv.js"></script>
-		<![endif]-->
-	</head>
-	<body class="home">
-		<!--[if lt IE 8]>
-			<p class="chromeframe">Votre navigateur internet est <strong>obsolet</strong>. S'il vous plait veuillez <a href="http://browsehappy.com/">mettre à jour votre navigateur</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">activez Google Chrome Frame</a> pour améliorer votre expérence.</p>
-		<![endif]-->
-
-		<!-- Debut du header -->
-		<header>
-			<section class="notification-zone">
-				<div class="notification center">
-					Montrouge & Moi est votre espace dans votre ville. Créer votre espace personnalisé en vous inscrivant :)
-					<a href="#" class="close ir">Fermer</a>
-				</div>
-			</section>
-			<section class="user">
-				<div class="center">
-					<div class="logo"><img src="images/logo.png" alt="Montrouge" /><div class="user-logo">& Loïc</div></div>
-					<div class="login">
-						<a href="#">Inscription</a> | <a href="#">Connexion</a>
-					</div>
-				</div>
-			</section>
-			<nav class="main-menu">
-				<span class="current">Accueil</span>
-				<a href="evenements.html">Évènements</a>
-				<a href="associations.html">Associations</a>
-				<form name="search" class="search" action="/evenements.php" method="get"><input type="text" class="search field" placeholder="Rechercher" /><input type="submit" class="search submit" value="Rechercher"></form>
-			</nav>
-		</header>
-		<!-- Fin du header -->
+<?php include_once "header.php"; ?>
 
 		<!-- Debut du corps du site -->
 		<div class="content group">
@@ -183,44 +137,4 @@
 		</div>
 		<!-- Fin du corps du site -->
 
-		<!-- Debut du footer -->
-		<footer class="group">
-			<div class="center">
-				<div class="sitemap">
-					<h3>Montrouge et moi</h3>
-					<nav class="footer-menu">
-						<ul>
-							<li></li>
-						</ul>
-					</nav>
-				</div>
-
-				<div class="top-associations">
-					<h3>Top associations</h3>
-					<ul>
-						<li></li>
-					</ul>
-				</div>
-
-				<div class="login">
-					<h3>Login</h3>
-					<input type="text" />
-					<input type="password" />
-				</div>
-			</div>
-		</footer>
-		<!-- Fin du footer -->
-
-
-		<!-- Debut des scripts. 
-		<script src="js/main.js"></script>
-
-		Google Analytics: ne pas oublier de mettre l'ID Correcte. 
-		<script>
-			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-			g.src='//www.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g,s)}(document,'script'));
-		</script> -->
-	</body>
-</html>
+<?php include_once "footer.php" ?>
