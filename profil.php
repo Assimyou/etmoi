b6199a671329a87d27aa201342b5c7b800453d46
Assimyou <assimyou.toure@orange.fr>
2013-07-23 23:04:02 +0200

    Ajout gestion d'accès

--- profil.php
+++ profil.php
@@ -1,6 +1,33 @@
 <?php
 session_start();
 include_once 'dbh.php';
-include_once 'controller/user/param.php';
-include_once 'view/profil.php';
+
+$access = FALSE;
+
+foreach ($visa as $key => $value) 
+{
+	if ($value == '') 
+	{
+		$id = $_GET['id'];
+		$access = TRUE;
+	}
+	else
+	{
+		if (!empty($_SESSION['id'])) 
+		{
+			$id = $_SESSION['id'];
+			$access = TRUE;
+		}
+	}
+}
+if ($access == TRUE) 
+{
+	include_once 'controller/user/param.php';
+	include_once 'view/profil.php';
+}
+else
+{
+	header('Location: index.php');
+	exit();
+}
 ?>
