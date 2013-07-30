ecee70b6d698af440c8be966dccb4a6035f4342a
Loïc Huck <contact@huckloic.fr>
2013-07-30 16:18:07 +0200

    ajout de la page even

---
+++ view/event.php
@@ -0,0 +1,17 @@
+<?php include_once "header.php"; ?>
+		<div class="content event group">
+			<div class="map"></div>
+			<section class="description center">
+				<h1>Barbecue des anciens élèves de Montrouge</h1>
+				<div class="registering">
+					<div class="registered">61 inscrits</div>
+					<a href="#register" class="btn register">J'y vais </a>
+				</div>
+				<p>Le seul aménagement cyclable en site propre de la ville se situe avenue Marx-Dormoy (D62), à cheval sur les communes de Montrouge et Arcueil. Il présente l'originalité d'être disposée au centre de la voirie, et deux bordures plantées la séparent de la circulation.</p>
+				<p>L'avenue de la Marne présente également une piste cyclable à double-sens sur un côté de la rue, y compris là où la vitesse de la circulation générale est limitée à 30 km/h. Des couloirs de bus autorisés aux vélos, notamment sur la RD920, permettent de faciliter leur circulation et, depuis sa rénovation achevée en novembre 2010, l'avenue Henri-Ginoux (D128) est dotée sur son côté droit d'une bande cyclable entre les avenues Gabriel-Péri et Verdier. La présence permanente de véhicules stationnés en infraction sur le côté gauche de cette section de l'avenue oblige cependant les autres véhicules à l'emprunter pour circuler.</p>
+				<p>Les nombreux feux de la commune ne sont pas dotés de sas pour les cyclistes. Les double-sens cyclables sont inexistants, et les zones 30 ont été supprimées en juillet 20112,3.</p>
+				<p>C'est à Montrouge que la juge Catherine Giudicelli est décédée des suites d'un accident à Vélib' en août 20094, premier accident mortel en banlieue parisienne en utilisant ce moyen de transport.</p>
+				<div class="category">Catégories : <a href="evenements?category=vide-grenier">Vide grenier</a> <a href="evenements?category=public">public</a></div>
+			</section>
+		</div>
+<?php include_once "footer.php"; ?>
