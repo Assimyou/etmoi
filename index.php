a3e3616ea09fc577f710317ed562577461efae29
Assimyou <assimyou.toure@orange.fr>
2013-08-04 17:02:04 +0200

    Ajout dans l'en-tête HTTP de no-cache

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 session_start();
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+
 ini_set('include_path', ini_get('include_path').';./model/;./view/;./controller/');
 
 include_once 'dbh.php';
 include_once 'user/authentication.php';
+session_write_close();
+
+include_once 'user/access.php';
+
 include_once 'view/home.php';
 ?>
