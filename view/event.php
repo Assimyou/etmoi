2448e0948ccafe1cdecbb8f582fa9db0625420af
Assimyou <assimyou.toure@orange.fr>
2013-08-04 16:54:00 +0200

    Affichage des informations

--- view/event.php
+++ view/event.php
@@ -4,7 +4,11 @@
 			<section class="description group center">
 				<div class="map"><div class="google"></div></div>
 				<article>
-					<h1>Barbecue des anciens élèves de Montrouge</h1>
+					<?php if (!empty($events['headline'])) : ?>
+					<?php foreach ($events['headline'] as $key => $value) : ?>
+					<h1><?php echo $value; ?></h1>
+					<?php endforeach; ?>
+					<?php endif; ?>
 					<div class="registering">
 						<div class="registered">61 inscrits</div>
 						<a href="#register" class="btn register">J'y vais </a>
@@ -13,7 +17,8 @@
 					<p>L'avenue de la Marne présente également une piste cyclable à double-sens sur un côté de la rue, y compris là où la vitesse de la circulation générale est limitée à 30 km/h. Des couloirs de bus autorisés aux vélos, notamment sur la RD920, permettent de faciliter leur circulation et, depuis sa rénovation achevée en novembre 2010, l'avenue Henri-Ginoux (D128) est dotée sur son côté droit d'une bande cyclable entre les avenues Gabriel-Péri et Verdier. La présence permanente de véhicules stationnés en infraction sur le côté gauche de cette section de l'avenue oblige cependant les autres véhicules à l'emprunter pour circuler.</p>
 					<p>Les nombreux feux de la commune ne sont pas dotés de sas pour les cyclistes. Les double-sens cyclables sont inexistants, et les zones 30 ont été supprimées en juillet 20112,3.</p>
 					<p>C'est à Montrouge que la juge Catherine Giudicelli est décédée des suites d'un accident à Vélib' en août 20094, premier accident mortel en banlieue parisienne en utilisant ce moyen de transport.</p>
-					<div class="category">Catégories : <a href="evenements?category=vide-grenier">Vide grenier</a> <a href="evenements?category=public">public</a></div>
+				
+				 <?php if (!empty($events['tag'])) : ?><div class="category">Catégories : <?php foreach ($events['tag'] as $key => $value) : ?><a href="evenements.php?q=<?php echo $value; ?>"><?php echo $value; ?></a><?php endforeach; ?></div><?php endif; ?>
 				</article>
 			</section>
 
