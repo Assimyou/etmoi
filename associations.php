f24767d4e1306d96dfebfb1692707316e96a7c2e
Assimyou <assimyou.toure@orange.fr>
2013-08-04 17:54:08 +0200

    Ajout dans l'en-tête HTTP de no-cache

--- associations.php
+++ associations.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+
 ini_set('include_path', ini_get('include_path').';./model/;./view/;./controller/');
 
 include_once 'dbh.php';
