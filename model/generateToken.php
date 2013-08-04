df30e224fa461bf4e3ed408adf50d5ca56a8a16f
Assimyou <assimyou.toure@orange.fr>
2013-08-04 16:42:16 +0200

    Ajout de la fonction generateur de Jeton de sécurité (Token)

---
+++ model/generateToken.php
@@ -0,0 +1,15 @@
+<?php
+function generateToken($name)
+{
+	session_start();
+
+	$token = uniqid(mt_rand());
+	$timeTokens = new DateTime('now');
+
+	$_SESSION[$name]['token'] = $token;
+	$_SESSION[$name]['time-tokens'] = $timeTokens;
+	session_write_close();
+
+	return $token;
+}
+?>
