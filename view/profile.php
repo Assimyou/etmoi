a26fa5f5a7311a77e174c9aa2a140e5dc872c4d0
Assimyou <assimyou.toure@orange.fr>
2013-08-23 21:48:21 +0200

    Modification page profil

--- view/profile.php
+++ view/profile.php
@@ -1,10 +1,33 @@
 <?php include_once "header.php"; ?>
 
+		<?php
+
+		function editbutton($token, $private){
+			global $ISME;
+
+			$editbutton = "";
+			if($ISME){
+				$privacy = 'Public';
+				if($private){ $privacy = 'Privé'; }
+				
+				$editbutton = 
+					'<div class="edit">'.
+						'<div class="privacy tooltip">'.$privacy.'</div>'.
+						'<a class="btn createform action="" method="post" " href="#">Éditer</a>'.
+						'<input type="submit" name="'.$token.'" value="enregistrer" class="btn editing" />'.
+					'</div>';
+			}
+
+			echo $editbutton;
+		}
+
+		?>
+
 		<!-- Debut du corps du site -->
 		<div class="content profil group">
 			<section class="cover" style="background-image:url('images/cover-cat.jpg')">
 				<div class="center">
-						<h1><?php if (!empty($users['firstname']) && !empty($users['name'])) : 
+					<h1><?php 	if (!empty($users['firstname']) && !empty($users['name'])) : 
 						 			if (!empty($users['firstname'])) : 
 						 				foreach ($users['firstname'] as $key => $value) : 
 						 					echo htmlentities($value, ENT_QUOTES); 
@@ -14,10 +37,10 @@
 						 				foreach ($users['name'] as $key => $value) : 
 						 					echo " ".htmlentities($value, ENT_QUOTES);
 						 				endforeach; 
-						 			endif; 
-						 		 else : 
+						 			endif;
+						 		else : 
 						 		 	echo 'Moi'; 
-						 		 endif; ?></h1>
+						 		endif; ?></h1>
 					<figure><img src="images/avatar.png" alt="" /></figure>
 				</div>
 			</section>
@@ -28,46 +51,120 @@
 						<a href="#events">Évènements</a>
 						<a href="#associations">Associations</a>
 						<a href="#">Badges</a>
-						<a href="#">Notifications</a>
+						<?php if($ISME) : ?><a href="#">Notifications</a><?php endif; ?>
 					</nav>
 				</aside>
 				<section class="col-3-2">
-					<?php if($isMe) : ?>
-					<section id="profil" class="profil group">
-						<h1 class="banner">Profil</h1>
-						<form>
-							<h2>E-Mail</h2><label>contact@huckloic.fr</label>
-							<h2>Mot de sécurité</h2><label>*******</label>
-						</form>
+					<?php if($ISME || $ISLEADER) : ?>
+					<!-- Début section profil -->
+					<section id="profil" class="profil showing group">
+						<form action="" method="post" >
+							<h1 class="banner">Profil</h1>
+							<?php 
+							editbutton($newToken, true); 
+
+							$showing = "";
+							$editing = "";
+
+							if (!empty($users['mail'])){
+								foreach ($users['mail'] as $key => $value){
+									$showing .= '<h2>E-Mail</h2><label>'.htmlentities($value, ENT_QUOTES).'</label>';
+									$editing .= '<h2>E-Mail</h2><input type="text" name="mail['.$key.']" value="'.htmlentities($value, ENT_QUOTES).'" placeholder="E-Mail" />';
+								}
+							} else {
+								$showing .= '';
+								$editing .= '<h2>E-Mail</h2><input type="text" name="mail[]" placeholder="E-Mail" />';
+							}
+							?>
+
+							<div class="showing">
+								<?php if($showing != "") : ?><?php echo $showing; ?><?php endif; ?>
+								<?php if($ISME) : ?><h2>Mot de sécurité</h2><label>********</label><?php endif; ?>
+							</div>
+
+							<?php if($ISME) : ?>
+							<div class="editing">
+								<?php if($editing != "") : ?><?php echo $editing; ?><?php endif; ?>
+								<h2>Mot de sécurité</h2><input type="password" name="password[]" placeholder="Mot de sécurité" />
+							</div>
+							<?php endif; ?>
+						</form action="" method="post" >
 					</section>
+					<!-- Fin section profil -->
 					<?php endif; ?>
 
-					<section id="identity" class="identity group">
-						<h1 class="banner">Identité</h1>
-						<div class="edit">
-						<form>
-							<div>
-								<div class="col-2">
-									<h2>Prénom</h2><label>Loïc</label>
+					<section id="identity" class="identity showing group">
+						<form action="" method="post" >
+							<h1 class="banner">Identité</h1>
+							<?php 
+							editbutton($newToken, false); 
+
+							$showing = "";
+							$editing = "";
+
+							if (!empty($users['firstname'])){
+								foreach ($users['firstname'] as $key => $value){
+									$showing .= '<h2>E-Mail</h2><label>'.htmlentities($value, ENT_QUOTES).'</label>';
+									$editing .= '<h2>E-Mail</h2><input type="text" name="mail['.$key.']" value="'.htmlentities($value, ENT_QUOTES).'" placeholder="Prénom" />';
+								}
+							} else {
+								$showing .= '';
+								$editing .= '<h2>E-Mail</h2><input type="text" name="mail[]" placeholder="Prénom" />';
+							}
+							?>
+
+							<div class="showing">
+								<div>
+									<div class="col-2">
+										<h2>Prénom</h2><label>Loïc</label>
+									</div>
+									<div class="col-2">
+										<h2>Nom</h2><label>Huck</label>
+									</div>
 								</div>
-								<div class="col-2">
-									<h2>Nom</h2><label>Huck</label>
+								<div>
+									<div class="col-2">
+										<h2>Sexe</h2><label>Homme</label>
+									</div>
+									<div class="col-2">
+										<h2>Date de naissance</h2><label>13/09/2013</label>
+									</div>
 								</div>
 							</div>
-							<div>
-								<div class="col-2">
-									<h2>Sexe</h2><label>Homme</label>
+
+							<?php if($ISME) : ?>
+							<div class="editing">
+								<div>
+									<div class="col-2">
+										<h2>Prénom</h2><input type="text" value="Loïc" />
+									</div>
+									<div class="col-2">
+										<h2>Nom</h2><input type="text" value="Huck" />
+									</div>
 								</div>
-								<div class="col-2">
-									<h2>Date de naissance</h2><label>13/09/2013</label>
+								<div>
+									<div class="col-2">
+										<h2>Sexe</h2>
+										<input type="radio" name="sexe[]" id="femme" value="Femme" /> 
+										<label for="femme">Femme</label> 
+										<input type="radio" name="sexe[]" id="homme" value="Homme" checked="true" /> 
+										<label for="homme">Homme</label> 
+										<input type="radio" name="sexe[]" id="autre" value="Autre" /> 
+										<label for="autre">Autre</label> 
+									</div>
+									<div class="col-2">
+										<h2>Date de naissance</h2><input type="date" />
+									</div>
 								</div>
 							</div>
-						</form>
+							<?php endif; ?>
+						</form action="" method="post" >
 					</section>
 
-					<section id="social" class="social group">
+					<section id="social" class="social showing group">
 						<h1 class="banner">Réseaux sociaux</h1>
-						<form>
+						<?php editbutton($newToken, false); ?>
+						<form action="" method="post" >
 							<div class="col-3">
 								<h2>Twitter</h2><label><span>@</span>loichuck</label>
 							</div>
@@ -77,20 +174,24 @@
 							<div class="col-3">
 								<h2>Instagram</h2><label><span>instagr.am/</span>loichuck</label>
 							</div>
-						</form>
+						</form action="" method="post" >
 					</section>
 
-					<section id="contact" class="contact group">
+					<?php if($ISME || $ISLEADER) : ?>
+					<section id="contact" class="contact showing group">
 						<h1 class="banner">Contacts</h1>
-						<form>
+						<?php editbutton(true, $newToken); ?>
+						<form action="" method="post" >
 							<h2>Adresse</h2><label>42, rue Barbès 94200 Ivry-sur-Seine, France</label>
 							<h2>Téléphone</h2><label class="col-2 tel">01 52 36 45 87</label><label class="col-2 mobile">06 72 23 68 26</label>
-						</form>
+						</form action="" method="post" >
 					</section>
+					<?php endif; ?>
 
-					<section id="say-more" class="say-more group">
+					<section id="say-more" class="say-more showing group">
 						<h1 class="banner">Dites en plus !</h1>
-						<form>
+						<?php editbutton(false, $newToken); ?>
+						<form action="" method="post" >
 							<h2>Description</h2><label>Failure is not an option. 
 							Where ignorance lurks, so too do the frontiers of discovery and imagination. 
 							Across the sea of space, the stars are other suns. 
@@ -101,7 +202,7 @@
 							<h2>Loisirs</h2><label>Art, Graphisme, Design, Web, Velo, Escalade, Jeux Vidéos</label>
 							<h2>Métier</h2><label>Graphiste Global</label>
 							<h2>Entreprise</h2><label>Société les fantastiques</label>
-						</form>
+						</form action="" method="post" >
 					</section>
 
 					<section id="events" class="group">
