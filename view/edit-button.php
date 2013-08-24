f399952154eef9648d3f90677b5fc39c9bbbf417
Assimyou <assimyou.toure@orange.fr>
2013-08-24 14:53:10 +0200

    edit button

---
+++ view/edit-button.php
@@ -0,0 +1,7 @@
+<!-- edit button -->
+<div class="edit">
+	<div class="privacy tooltip"><?php echo $visibility; ?></div>
+	<a class="btn createform" href="#">Éditer</a>
+	<input type="submit" name="<?php echo $newToken; ?>" value="enregistrer" class="btn editing" />
+</div>
+<!-- end button -->
