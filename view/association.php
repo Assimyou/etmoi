ed531c744923659e61225ce6c24a4f6cafb53100
Assimyou <assimyou.toure@orange.fr>
2013-08-25 20:26:50 +0200

    Ajout de la description dans <p>

--- view/association.php
+++ view/association.php
@@ -4,18 +4,25 @@
 		<div class="content association group">
 			<section class="description group">
 				<div class="cover" style="background-image: url('images/vide-grenier.jpg');">
-					<div class="center"><h1>Le club du parc</h1></div>
+					<?php if (!empty($associations['name'])) : ?><div class="center"><?php foreach ($associations['name'] as $key => $value) : ?><h1><?php echo $value; ?></h1><?php endforeach; ?></div><?php endif; ?>
 				</div>
 				<article class="center">
 					<div class="registering">
 						<div class="registered">61 membres</div>
 						<a href="#register" class="btn register">J'adhère </a>
 					</div>
-					<p>Le seul aménagement cyclable en site propre de la ville se situe avenue Marx-Dormoy (D62), à cheval sur les communes de Montrouge et Arcueil. Il présente l'originalité d'être disposée au centre de la voirie, et deux bordures plantées la séparent de la circulation.</p>
-					<p>L'avenue de la Marne présente également une piste cyclable à double-sens sur un côté de la rue, y compris là où la vitesse de la circulation générale est limitée à 30 km/h. Des couloirs de bus autorisés aux vélos, notamment sur la RD920, permettent de faciliter leur circulation et, depuis sa rénovation achevée en novembre 2010, l'avenue Henri-Ginoux (D128) est dotée sur son côté droit d'une bande cyclable entre les avenues Gabriel-Péri et Verdier. La présence permanente de véhicules stationnés en infraction sur le côté gauche de cette section de l'avenue oblige cependant les autres véhicules à l'emprunter pour circuler.</p>
-					<p>Les nombreux feux de la commune ne sont pas dotés de sas pour les cyclistes. Les double-sens cyclables sont inexistants, et les zones 30 ont été supprimées en juillet 20112,3.</p>
-					<p>C'est à Montrouge que la juge Catherine Giudicelli est décédée des suites d'un accident à Vélib' en août 20094, premier accident mortel en banlieue parisienne en utilisant ce moyen de transport.</p>
-				
+					<?php if (!empty($associations['description'])) : ?>
+						<?php foreach ($associations['description'] as $key => $value) : ?>
+
+						<?php $paragraph = mb_split('\r\n', $value);
+
+								foreach ($paragraph as $p) :
+								if (!empty($p)) : ?>
+					<p><?php echo htmlentities($p, ENT_QUOTES); ?></p>
+								<?php endif;
+								endforeach;
+						  	endforeach;
+						endif; ?>
 				 <?php if (!empty($events['tag'])) : ?><div class="category">Catégories : <?php foreach ($events['tag'] as $key => $value) : ?><a href="evenements.php?q=<?php echo $value; ?>"><?php echo $value; ?></a><?php endforeach; ?></div><?php endif; ?>
 				</article>
 			</section>
