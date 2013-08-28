9e15342acb00b2617aa1219a146f85dbb5fc4f08
Assimyou <assimyou.toure@orange.fr>
2013-08-28 18:50:21 +0200

    Association

--- view/association-form.php
+++ view/association-form.php
@@ -3,10 +3,10 @@
 		<!-- Debut du corps du site -->
 		<div class="content center group">
 			<section>
-				<form action=""enctype="multipart/form-data"  method="post" />
+				<form action="" enctype="multipart/form-data" method="post" >
 					<?php if (!empty($associations['name'])) : ?>
 					<?php foreach ($associations['name'] as $key => $value) : ?>
-					<input type="text" name="name[<?php echo $key; ?>]" value="<?php echo $value; ?>" placeholder="association" />
+					<input type="text" name="name[<?php echo $key; ?>]" value="<?php echo htmlentities($value, ENT_QUOTES); ?>" placeholder="association" />
 					<?php endforeach; ?>
 					<?php else : ?>
 					<input type="text" name="name[]" placeholder="association" />
@@ -14,7 +14,7 @@
 
 					<?php if (!empty($events['description'])) : ?>
 					<?php foreach ($events['description'] as $key => $value) : ?>
-					<textarea name="description[<?php echo $key; ?>]" placeholder="description" ><?php echo $value; ?></textarea>
+					<textarea name="description[<?php echo $key; ?>]" placeholder="description" ><?php echo htmlentities($value, ENT_QUOTES); ?></textarea>
 					<?php endforeach; ?>
 					<?php else : ?>
 					<textarea name="description[]" placeholder="description" ></textarea>
@@ -26,7 +26,7 @@
 
 					<?php if (!empty($associations['zip'])) : ?>
 					<?php foreach ($associations['zip'] as $key => $value) : ?>
-					<input type="text" name="zip[<?php echo $key; ?>]" value="<?php echo $value; ?>" placeholder="Code postal" />
+					<input type="text" name="zip[<?php echo $key; ?>]" value="<?php echo htmlentities($value, ENT_QUOTES); ?>" placeholder="Code postal" />
 					<?php endforeach; ?>
 					<?php else : ?>
 					<input type="text" name="zip[]" placeholder="Code postal" />
@@ -34,7 +34,7 @@
 
 					<?php if (!empty($associations['city'])) : ?>
 					<?php foreach ($associations['city'] as $key => $value) : ?>
-					<input type="text" name="city[<?php echo $key; ?>]" value="<?php echo $value; ?>" placeholder="Ville" />
+					<input type="text" name="city[<?php echo $key; ?>]" value="<?php echo htmlentities($value, ENT_QUOTES); ?>" placeholder="Ville" />
 					<?php endforeach; ?>
 					<?php else : ?>
 					<input type="text" name="city[]" placeholder="ville" />
@@ -42,7 +42,7 @@
 
 					<?php if (!empty($associations['address'])) : ?>
 					<?php foreach ($associations['address'] as $key => $value) : ?>
-					<input type="text" name="address[<?php echo $key; ?>]" value="<?php echo $value; ?>" placeholder="location" />
+					<input type="text" name="address[<?php echo $key; ?>]" value="<?php echo htmlentities($value, ENT_QUOTES); ?>" placeholder="location" />
 					<?php endforeach; ?>
 					<?php else : ?>
 					<input type="text" name="address[]" placeholder="location" />
@@ -50,13 +50,13 @@
 
 					<?php if (!empty($associations['date'])) : ?>
 					<?php foreach ($associations['date'] as $key => $value) : ?>
-					<input type="text" name="date[<?php echo $key; ?>]" value="<?php echo $value; ?>" placeholder="date" />
+					<input type="text" name="date[<?php echo $key; ?>]" value="<?php echo htmlentities($value, ENT_QUOTES); ?>" placeholder="date" />
 					<?php endforeach; ?>
 					<?php else : ?>
 					<input type="text" name="date[]" placeholder="date" />
 					<?php endif; ?>
 
-					<input type="submit" name="<?php echo generateToken($_SERVER['REQUEST_URI']); ?>" value="enregistrer" />
+					<input type="submit" name="association-<?php echo generateToken($_SERVER['REQUEST_URI']); ?>" value="enregistrer" />
 				</form>
 			</section>
 		</div>
