aebc3714958b9d4426fc44a948d051d6c6f1ee1a
Elwann <lojic@hotmail.fr>
2013-08-29 20:26:23 +0200

    php associations

--- view/association-search.php
+++ view/association-search.php
@@ -1,37 +1,36 @@
 <?php include_once "view/header.php"; ?>
 	
+	<div class="search-zone group">
+		<form action="" method="get" id="search" />
+			<input type="text" name="q" placeholder="rechercher une association" />
+			<input type="submit" id="research" value="rechercher" class="btn" />
+		</form>
+	</div>
+
 	<!-- Debut du corps du site -->
-	<div>
-
-		<section>
-			<form action="" method="get" id="search" />
-				<input type="text" name="q" placeholder="rechercher une association" />
-				<input type="submit" id="research" value="rechercher" />
-			</form>
-		</section>
-
+	<div class="content association group">
 		<?php if (!empty($listAssos)) : ?>
-		<section>
-			<h1>Les associations</h1>
-				<div class="wrapper">
-					<?php foreach ($listAssos as $id => $associations) : ?>
-					<article class="association">
-						<?php if (!empty($associations['name'])) : ?>
-						<?php foreach ($associations['name'] as $index => $name) : ?>
-						<h2><?php echo $name; ?></h2>
-						<?php endforeach; ?>
-	  					<?php endif; ?>
-	  					<?php if (!empty($associations['illustration'])) : ?>
-	  					<div class="thumbnail">
-						<?php foreach ($associations['illustration'] as $index => $illustration) : ?>
-							<img src="<?php echo $illustration; ?>" alt="Logo du club du parc" />
-						<?php endforeach; ?>
-						</div>
-	  					<?php endif; ?>
-						<a class="more" href="association.php?q=<?php echo $id; ?>"><span class="plus ir">Plus</span> de détails</a>
-					</article>
+		<section class="center group">
+			<h1 class="banner">Les associations</h1>
+			<div class="wrapper">
+				<?php foreach ($listAssos as $id => $associations) : ?>
+				<article class="association float-3">
+					<?php if (!empty($associations['name'])) : ?>
+					<?php foreach ($associations['name'] as $index => $name) : ?>
+					<h2><a href="association.php?q=<?php echo $id; ?>"><?php echo $name; ?></a></h2>
 					<?php endforeach; ?>
-				</div>
+  					<?php endif; ?>
+  					<?php if (!empty($associations['illustration'])) : ?>
+  					<figure>
+					<?php foreach ($associations['illustration'] as $index => $illustration) : ?>
+						<a href="association.php?q=<?php echo $id; ?>"><img src="<?php echo $illustration; ?>" alt="Logo du club du parc" /></a>
+					<?php endforeach; ?>
+					</figure>
+  					<?php endif; ?>
+					<a class="more" href="association.php?q=<?php echo $id; ?>"><span class="plus ir">Plus</span> de détails</a>
+				</article>
+				<?php endforeach; ?>
+			</div>
 		</section>
 		<?php endif; ?>
 
